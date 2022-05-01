63be8e274a34c1aefdc69da803e0c00968a90c9f
Nick Korotysh <nick.korotysh@gmail.com>
2022-05-01 14:22:51 +0300

    improved demo app

--- gradient_dialog_demo.cc
+++ gradient_dialog_demo.cc
@@ -25,6 +25,15 @@ int main(int argc, char* argv[])
 {
   QApplication app(argc, argv);
   GradientDialog dlg;
+  QConicalGradient g(0.5, 0.5, 45.0);
+  g.setStops({
+    {0.00, {170,   0,   0}},  // #aa0000
+    {0.20, {  0,  85, 255}},  // #0055ff
+    {0.45, {  0, 170,   0}},  // #00aa00
+    {0.65, {255, 255,   0}},  // #ffff00
+    {1.00, {170,   0,   0}},  // #aa0000
+  });
+  dlg.setGradient(g);
   dlg.show();
   return app.exec();
 }
