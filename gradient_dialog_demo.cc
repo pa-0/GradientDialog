ecab39dc8c7e5a65bad3f5cf45bc3f6876bf71dc
Nick Korotysh <nick.korotysh@gmail.com>
2024-01-27 00:17:08 +0100

    use SPDX license identifier

--- gradient_dialog_demo.cc
+++ gradient_dialog_demo.cc
@@ -1,22 +1,8 @@
 /*
-  gradient picker dialog demo application
-  Copyright (C) 2022  Nick Korotysh <nick.korotysh@gmail.com>
-
-  This file is part of Qt gradient picker library.
-
-  Qt gradient picker library is free software: you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public License as
-  published by the Free Software Foundation, either version 3 of the License,
-  or (at your option) any later version.
-
-  Qt gradient picker library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
-  for more details.
-
-  You should have received a copy of the GNU Lesser General Public License along
-  with Qt gradient picker library. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * SPDX-FileCopyrightText: 2022 Nick Korotysh <nick.korotysh@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
 
 #include <QApplication>
 #include "gradient_dialog.h"
